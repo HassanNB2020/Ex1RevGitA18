38f4fcfc31deee7e4e93aeb8cf489cc9a13204eb
Zaki <Zaki@DESKTOP-L8JKFI7>
2021-09-07 20:33:45 -0400

    changement classe Examen

--- src/model/Examen.java
+++ src/model/Examen.java
@@ -10,5 +10,38 @@ package model;
  * @author Zaki
  */
 public class Examen {
+    private String examTheorique;
+    private String examPratique;
+
+    public Examen() {
+    }
+
+    public Examen(String examTheorique, String examPratique) {
+        this.examTheorique = examTheorique;
+        this.examPratique = examPratique;
+    }
+
+    public String getExamTheorique() {
+        return examTheorique;
+    }
+
+    public void setExamTheorique(String examTheorique) {
+        this.examTheorique = examTheorique;
+    }
+
+    public String getExamPratique() {
+        return examPratique;
+    }
+
+    public void setExamPratique(String examPratique) {
+        this.examPratique = examPratique;
+    }
+
+    @Override
+    public String toString() {
+        return "Examen{" + "examTheorique=" + examTheorique + ", examPratique=" + examPratique + '}';
+    }
+    
+    
     
 }
