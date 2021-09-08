a2c1b7550cb15697f88c070f61602fb24685d734
User <User@DESKTOP-LOK1PVC>
2021-09-07 20:14:44 -0400

    ajout de classe cours avec constructeur

--- src/model/Cours.java
+++ src/model/Cours.java
@@ -10,5 +10,43 @@ package model;
  * @author User
  */
 public class Cours {
+    private String codeCours;
+    private String nomCours;
+    private String Salle;
+
+    public Cours(String codeCours, String nomCours, String Salle) {
+        this.codeCours = codeCours;
+        this.nomCours = nomCours;
+        this.Salle = Salle;
+    }
+
+    public String getCodeCours() {
+        return codeCours;
+    }
+
+    public void setCodeCours(String codeCours) {
+        this.codeCours = codeCours;
+    }
+
+    public String getNomCours() {
+        return nomCours;
+    }
+
+    public void setNomCours(String nomCours) {
+        this.nomCours = nomCours;
+    }
+
+    public String getSalle() {
+        return Salle;
+    }
+
+    public void setSalle(String Salle) {
+        this.Salle = Salle;
+    }
+
+    @Override
+    public String toString() {
+        return "Cours{" + "codeCours=" + codeCours + ", nomCours=" + nomCours + ", Salle=" + Salle + '}';
+    }
     
 }
